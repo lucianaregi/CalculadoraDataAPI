f8cc1fc02b493ac62f144da61e001c6668d48a77
lucianaregi <lucianaregi@gmail.com>
2021-03-14 12:16:49 -0300

    Add project files.

---
+++ CalculadoraDataAPI/Controllers/CalcularDataController.cs
@@ -0,0 +1,69 @@
+﻿using CalculadoraDataAPI.Helper;
+using CalculadoraDataAPI.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculadoraDataAPI.Controllers
+{
+    [Route("api/v1/calculardata")]
+    [ApiController]
+    public class CalcularDataController : ControllerBase
+    {
+        private readonly ConversorCalendarioService _conversorCalendario;
+
+        /// <summary>
+        /// Construtor
+        /// </summary>
+        /// <param name="conversorCalendario"></param>
+        public CalcularDataController(ConversorCalendarioService conversorCalendario) => _conversorCalendario = conversorCalendario;
+
+        /// <summary>
+        /// Método que retorna o cáclulo da data
+        /// </summary>
+        /// <param name="data">dd/mm/yyyy hh:mm</param>
+        /// <param name="operacao">operador matemático soma (+) ou subtração (-)</param>
+        /// <param name="valor"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Get([FromQuery] string data, [FromQuery] string operacao, [FromQuery] long valor)
+        {
+            string date = string.Empty;
+            string message = string.Empty;
+            bool _data = Util.DateValidate(data);
+            valor = Util.PositiveNumber(valor);
+            bool _valor = Util.IsValid(valor);
+            bool _operacao = Util.OperatorValidate(operacao);
+
+            try
+            {
+                if (_data && _valor && _operacao)
+                {
+                    var op = operacao;
+                    var valueDate = data.Split('/');
+                    var day = valueDate[0];
+                    var month = valueDate[1];
+                    var year = valueDate[2].Split(' ')[0];
+                    var hour = valueDate[2].Split(' ')[1].Split(':')[0];
+                    var minute = valueDate[2].Split(' ')[1].Split(':')[1];
+
+                    double dateJulian = _conversorCalendario.JulianDate(Int32.Parse(year), Int32.Parse(month), Int32.Parse(day), Int32.Parse(hour), Int32.Parse(minute), op, valor);
+                    date = _conversorCalendario.JulianToGregorian(dateJulian);
+
+                    return Ok(date);
+                }
+                else
+                {
+                    return BadRequest("Dados inválidos!");
+                }
+            }
+            catch (Exception)
+            {
+                return BadRequest("Ocorreu um erro na sua solicitação. Por favor, tente mais tarde!");
+            }
+        }
+    }
+}
---
+++ CalculadoraDataAPI/Startup.cs
@@ -0,0 +1,73 @@
+using CalculadoraDataAPI.Service;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace CalculadoraDataAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddScoped<ConversorCalendarioService>();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("calculadoradataapi", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title = "Calculadora Data API",
+                    Version = "1.0"
+                });
+
+                var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+
+                options.IncludeXmlComments(xmlCommentsFullPath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/calculadoradataapi/swagger.json", "CalculadoraDataAPI - 1.0");
+                options.RoutePrefix = "";
+            });
+        }
+    }
+}
